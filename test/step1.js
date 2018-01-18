2ea5648b65b461eb413873d51ec367bf5dc30ca6
Andy-Rong <727346595@qq.com>
2018-01-19 00:30:33 +0800

    del test folder

--- test/step1.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Andy on 2017/11/6.
- */
-
-let StorageRegister  = require("../public/plugin/StorageRegister/StorageRegister.js");
-
-let rg = new StorageRegister({foo:"congrssssss"});
-
-module.exports = rg;
