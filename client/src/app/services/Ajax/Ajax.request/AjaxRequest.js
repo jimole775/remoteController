42991ecd130d32ee521e9782a2ad4bf5c9067a36
Andy-Rong <727346595@qq.com>
2018-01-21 12:26:18 +0800

    reset exworked

--- client/src/app/services/Ajax/Ajax.request/AjaxRequest.js
+++ client/src/app/services/Ajax/Ajax.request/AjaxRequest.js
@@ -86,7 +86,7 @@ export default class AjaxRequest{
             type: "POST",
             async: true,
             timeout: 10000, //超时时间设置，单位毫秒
-            url: "http://127.0.0.1:80/" + pathName,
+            url: "http://127.0.0.1:1110/" + pathName,
             dataType: "json",
             data: querySearch,
             complete: function (XMLHttpRequest, status) {
