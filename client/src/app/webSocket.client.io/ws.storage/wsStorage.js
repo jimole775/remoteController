fee971c19a4b0e6f154f0a76c909426cb0b5c7ca
jimole775 <727346595@qq.com>
2019-08-13 13:05:14 +0800

    update

--- client/src/app/webSocket.client.io/ws.storage/wsStorage.js
+++ client/src/app/webSocket.client.io/ws.storage/wsStorage.js
@@ -16,7 +16,7 @@ let storage = {
     remoteChanelMap:[],  // 标记正在进行远程通讯业务的用户
     name:"userStorage",  // 标记正在进行远程通讯业务的用户
     // hostUrl:"lester.ink",  // 标记正在进行远程通讯业务的用户
-    hostUrl:"www.rongxis.com",  // 标记正在进行远程通讯业务的用户
+    hostUrl:"47.101.67.149",  // 标记正在进行远程通讯业务的用户
     httpPort:"8080",
     wsPort:"8081"
 };
